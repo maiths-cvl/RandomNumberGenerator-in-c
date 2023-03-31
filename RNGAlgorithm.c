7ce4bddaaccd076121b2ce80b22869e408034873
Capitaine-Vole <capitainevole@gmail.com>
2023-03-31 08:55:16 +0100

    RNG

---
+++ RNGAlgorithm.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+
+
+int main(int argc, char **argv){
+
+	if (strcmp(argv[1], "help") == 0){
+		printf("You can add a personalize seed by adding it just after %s", argv[0]);
+	}
+
+	if(argv[1])
+
+	return 0;
+}
